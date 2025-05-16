955ba55ea1aeab5561c953765559e3dfb04e8e30
Luk Ole <138208089+lukolee@users.noreply.github.com>
2025-05-16 16:33:37 +1000

    index page done

--- js/components.js
+++ js/components.js
@@ -1,3 +1,4 @@
+// load a html snippet from a file url
 function loadComponent(id, url) {
     fetch(url)
         .then(res => {
@@ -10,6 +11,8 @@ function loadComponent(id, url) {
         .catch(err => console.error(`Could not load ${url}: ${err}`));
 }
 
+// allow for laoding html snippets / images into a page and attach to
+// an element with id "navbar"
 function loadLayout() {
     loadComponent('navbar', 'js/navbar.html');
     // loadComponent('footer', 'footer.html');
